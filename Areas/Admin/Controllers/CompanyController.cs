f997207d9492e4b77d4069f98966301010ebd880
BasmallaMabrouk <basmallamabrouk@gmail.con>
2025-08-14 01:32:58 +0300

    Fixed Company View

--- Areas/Admin/Controllers/CompanyController.cs
+++ Areas/Admin/Controllers/CompanyController.cs
@@ -80,6 +80,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         }
 
 
+
         [HttpDelete]
         public IActionResult Delete(int? id)
         {
--- wwwroot/js/Company.js
+++ wwwroot/js/Company.js
@@ -1,25 +1,24 @@
 ﻿var dataTable;
+
 $(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-   dataTable= $('#myTable').DataTable({  
-        "ajax": {
-            "url": "/Admin/Product/GetAll"
-        },
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/company/getall' },
         "columns": [
-            { "data": "Name", "width": "25%" },
-            { "data": "PhoneNumber", "width": "15%" },
-            { "data": "City", "width": "10%" },
-            { "data": "StreetAddress", "width": "15%" },
-            { "data": "State", "width": "10%" },
+            { "data": "name", "width": "15%" },
+            { "data": "streetAddress", "width": "15%" },
+            { "data": "city", "width": "15%" },
+            { "data": "state", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
             {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/Company/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
-                     <a onClick=Delete('/admin/Company/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                     <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a onClick=Delete('/admin/company/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
                 "width": "25%"
@@ -27,15 +26,16 @@ function loadDataTable() {
         ]
     });
 }
+
 function Delete(url) {
     Swal.fire({
-        title: "Are you sure?",
+        title: 'Are you sure?',
         text: "You won't be able to revert this!",
-        icon: "warning",
+        icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
         if (result.isConfirmed) {
             $.ajax({
@@ -47,5 +47,5 @@ function Delete(url) {
                 }
             })
         }
-    });
+    })
 }
